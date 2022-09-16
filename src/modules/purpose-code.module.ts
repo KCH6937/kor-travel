41c38784321172526809fb989b7c2c28fe98b16a
KCH6937 <adasddasd12@naver.com>
2022-09-16 18:41:58 +0900

    [Feat] - 관광 목적 모듈 추가

---
+++ src/modules/purpose-code.module.ts
@@ -0,0 +1,13 @@
+const purposeMap: Map<string, number> = new Map([
+  ['없음', 0],
+  ['이국적인 문화/환경', 1],
+  ['휴양하기 좋은 곳', 2],
+  ['캠핑', 3],
+  ['아름다운 자연 경관', 4],
+  ['먹거리', 5],
+  ['레저', 6],
+]);
+
+module.exports = {
+  purposeMap,
+};
